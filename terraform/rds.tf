4030f95b1d70347d52375bb3b7880f97fe9899de
Ranjit-08 <ranjitsamalarj@gmail.com>
2026-03-12 12:09:49 +0530

    changes some code

--- terraform/rds.tf
+++ terraform/rds.tf
@@ -18,10 +18,6 @@ resource "aws_security_group" "rds" {
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
-  tags = {
-    Name = "listenme-rds-sg"
-  }
 }
 
 # ─── RDS Subnet Group ─────────────────────────────────────────────
